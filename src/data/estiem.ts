b17f8d45e0325b19643f3258cd0f457592e5b5a4
David Barrero <davbarfer@gmail.com>
2024-12-22 15:15:57 +0100

    Update project data

--- src/data/estiem.ts
+++ src/data/estiem.ts
@@ -1,3 +1,5 @@
+import { preview } from "astro"
+
 export const estiem = {
   title: 'ESTIEM',
   description: 'ESTIEM is a non-profit organization for Industrial Engineering and Management (IEM) students who combine technological understanding with management skills. Founded in 1990, our goal is to foster relations between IEM students and support their professional and personal development.',
@@ -7,3 +9,17 @@ export const position =[
   'Public Relations Responsible',
   'IT Committee Leader',
 ]
+
+export const projectESTIEMLGSeville = {
+  title: 'ESTIEM LG Seville',
+  description: 'ESTIEM LG Seville is a non-profit organization for Industrial Engineering and Management (IEM) students. It is part of the ESTIEM network. Created from scratch using vue.js and vuetify, the website is designed to provide information about the organization and its projects.',
+  src: '@/assets/img/project-01.webp',
+  preview: 'https://estiemlgseville.org/'
+}
+
+export const projectESTIEM = {
+  title: 'ESTIEM',
+  description: 'I develop components for the ESTIEM website as a frontend developer. The website is in vue.js and uses bootstrap for styling.',
+  src: '@/assets/img/project-02.webp',
+  preview: 'https://estiem.org/'
+}
