6ba819a7cdce7fabb3b8563c6dc6f85439f26da9
David Barrero <davbarfer@gmail.com>
2024-12-23 18:06:20 +0100

    Update data

--- src/data/engineer.ts
+++ src/data/engineer.ts
@@ -1,4 +1,4 @@
 export const engineer = {
 	title: 'Industrial Engineer',
-	description: 'I am a student of Industrial Engineering. I aim to create a positive impact on people and leave a meaningful imprint through my work. I focus on developing myself and the team by collaborating as one.',
+	description: 'I aim to create a positive impact on people and leave a meaningful imprint through my work. I focus on developing myself and the team by collaborating as one. During my student years I specialized in automation and electronics. I also invested my time in non-profit organizations.',
 };
