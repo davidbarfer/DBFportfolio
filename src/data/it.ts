577fe9f272fabe4a087f1a215eef7aa935ad274f
David Barrero <davbarfer@gmail.com>
2025-03-10 11:37:23 +0100

    Update github link

--- src/data/it.ts
+++ src/data/it.ts
@@ -1,7 +1,7 @@
 export const projectLinkPage = {
   title: 'Link Page',
   description: 'My personal link page. I use tailwindcss and vue.js.',
-  code: 'https://github.com/David-ByHunter/link-page',
+  code: 'https://github.com/davidbarfer/link-page',
   src: '@/assets/img/project-03.webp',
   preview: 'https://links.davidbarfer.com/'
 }
